771564df008b1d01b8c127bf39d25bd927fd9865
Tomiwajin <67241225+Tomiwajin@users.noreply.github.com>
2025-03-06 17:40:11 -0500

    Added Task-Manager-App to code folder

---
+++ Code/Task-Manager-App/src/main/java/edu/westga/comp4420/task_manager/model/Task.java
@@ -0,0 +1,5 @@
+package edu.westga.comp4420.task_manager.model;
+
+public class Task {
+
+}
---
+++ Code/Task-Manager-App/src/test/java/edu/westga/comp4420/task_manager/test/model/task/TestConstructor.java
@@ -0,0 +1,5 @@
+package edu.westga.comp4420.task_manager.test.model.task;
+
+public class TestConstructor {
+
+}
