090b8c996fe2c75aa23fe973bf8442b7e9c8f5ba
Tomiwajin <67241225+Tomiwajin@users.noreply.github.com>
2025-03-06 22:10:06 -0500

    Update MainWindow.java
    Implemented code for delete button

--- Code/Task-Manager-App/src/main/java/edu/westga/comp4420/task_manager/view/codebehind/MainWindow.java
+++ Code/Task-Manager-App/src/main/java/edu/westga/comp4420/task_manager/view/codebehind/MainWindow.java
@@ -8,6 +8,7 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -42,6 +43,10 @@ public class MainWindow {
 
     @FXML
     private Button editTaskButton;
+    
+    @FXML
+    private Button deleteTaskButton;
+
 
     private ObservableList<Task> taskList;
 
@@ -136,5 +141,31 @@ public class MainWindow {
             errorBox.setContentText("Unable to open edit task window");
             errorBox.showAndWait();
         }
+    }
+    
+    @FXML
+    private void handleDeleteTaskButtonAction() {
+        Task selectedTask = this.taskTable.getSelectionModel().getSelectedItem();
+        
+        if (selectedTask == null) {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("No Task Selected");
+            alert.setHeaderText(null);
+            alert.setContentText("Please select a task to delete.");
+            alert.showAndWait();
+            return;
+        }
+
+        Alert confirmationAlert = new Alert(Alert.AlertType.CONFIRMATION);
+        confirmationAlert.setTitle("Delete Task");
+        confirmationAlert.setHeaderText(null);
+        confirmationAlert.setContentText("Are you sure you want to delete this task?");
+
+        confirmationAlert.showAndWait().ifPresent(response -> {
+            if (response == ButtonType.OK) {
+                this.taskList.remove(selectedTask);
+                this.taskTable.refresh();
+            }
+        });
     }
 }
