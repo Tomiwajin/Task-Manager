9a64046a201cbf28926e526ba2953198e4084e26
Tomiwajin <oluwatomiwajinadu@gmail.com>
2025-05-05 02:55:46 -0400

    corrected Typo

--- Code/Task-Manager-App/website/app.js
+++ Code/Task-Manager-App/website/app.js
@@ -13,7 +13,7 @@ app.get('/', function(req, res) {
 });
 
 // route to about page
-app.get('/downlad', function(req, res) {
+app.get('/download', function(req, res) {
 	res.render('page/download');
 });
 
